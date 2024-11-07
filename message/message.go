18f9678b6808d4f724e4db8e6514b7459b8579c2
mishakrpv <mishavkrpv@gmail.com>
2024-11-07 03:22:08 +0300

    chore: add message test

--- message/message.go
+++ message/message.go
@@ -11,12 +11,12 @@ func MakeMessage() Message {
 	return make(map[string]interface{})
 }
 
-func (m *Message) Add(key string, value interface{}) Message {
-	(*m)[key] = value
-	return *m
+func (m Message) Add(key string, value interface{}) Message {
+	m[key] = value
+	return m
 }
 
-func (m *Message) Build() string {
+func (m Message) Build() string {
 	data, err := json.Marshal(m)
 	if err != nil {
 		log.Println("An error occurred while marshaling JSON:", err)
--- message/message_test.go
+++ message/message_test.go
@@ -1,1 +1,37 @@
 package message
+
+import (
+	"testing"
+)
+
+func TestMakeMessage(t *testing.T) {
+	msg := MakeMessage()
+	if len(msg) != 0 {
+		t.Errorf("expected empty message, got %v", msg)
+	}
+}
+
+func TestAdd(t *testing.T) {
+	msg := MakeMessage()
+	msg.Add("key1", "value1").Add("key2", 123)
+
+	if val, ok := msg["key1"]; !ok || val != "value1" {
+		t.Errorf("expected key1 to have value 'value1', got %v", val)
+	}
+
+	if val, ok := msg["key2"]; !ok || val != 123 {
+		t.Errorf("expected key2 to have value 123, got %v", val)
+	}
+}
+
+func TestBuild(t *testing.T) {
+	msg := MakeMessage()
+	msg.Add("key1", 123).Add("key2", MakeMessage().Add("key3", "value1"))
+
+	expected := `{"key1":123,"key2":{"key3":"value1"}}`
+	result := msg.Build()
+
+	if expected != result {
+		t.Fatalf("expected %s, got %s", expected, result)
+	}
+}
